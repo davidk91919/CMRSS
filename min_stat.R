fc42610c528a0447deac06ad1b7bf1644a6cb9da
davidk91919 <100812003+davidk91919@users.noreply.github.com>
2023-05-15 14:05:22 -0500

    Add files via upload

---
+++ min_stat.R
@@ -0,0 +1,43 @@
+#' @title Minimum stat value in H_{k,c}
+#' 
+#' @description 
+#' 
+#' @param Z Given assignment vector
+#' @param Y Outcome vector
+#' @param k k specified as kth largest treatment effect in the null hypothesis 
+#' @param c c specified as upper bound of kth largest treatment effect in the null hypothesis
+#' @param method.list list of method. 
+#' @param score user-specified score vector, if available
+#' @param ind.sort.treat user-specified alignment in treatment vector, if available
+
+
+min_stat <- function(Z, Y, k, c, method.list = NULL, 
+                     score = NULL, 
+                     ind.sort.treat = NULL){
+  n = length(Y)
+  m = sum(Z)
+  
+  if(is.null(method.list)){   #added
+    score= scale(score)
+  }
+  
+  if(is.null(score)){
+    score = rank_score(n, method.list)
+  }
+  
+  # sort the treated units
+  if(is.null(ind.sort.treat)){
+    ind.sort.treat = sort_treat(Y, Z)
+  }
+  
+  # get xi vector
+  xi = rep(c, n)
+  if(k < n){
+    xi[ ind.sort.treat[ ( m + 1 - min(m, n-k) ):m ] ] = Inf
+  }
+  
+  stat.min = sum( score[ rank( Y - Z * xi, ties.method = "first" )[Z==1] ] )
+  stat.min
+  
+  return(stat.min)
+}
