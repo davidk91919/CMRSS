3e9598bbddfd82feb8271b5485840f97f4018f5e
davidk91919 <100812003+davidk91919@users.noreply.github.com>
2023-10-09 13:17:06 -0500

    Add files via upload

--- power_analysis.R
+++ power_analysis.R
@@ -1,5 +1,6 @@
-
+###############
 ### Function for plotting heatmap, corresponding to multiple k(quantile of individual treatment effect) and c(upper bound on the null)
+###############
 
 heatmap_pval = function(Z, Y, 
                         k.vec, ## vector corresponds to quantiles of interest
@@ -39,37 +40,78 @@ heatmap_pval = function(Z, Y,
            cluster_cols = FALSE)    ## omitted clustering. can add an option for this
 }
 
+###############
+## Function for power simulation, treating estimated quantile treatment effects as a true treatment effects. 
+## rq function in quantreg package is used for estimating quantile treatment effect.
+## This is for binary treatment, multiple(possibly not equal size of) strata
+## usual difference in mean test statistic and our new test statistic are compared
+###############
 
 
-## function for power simulation; similar to 270p of the book. 
-## working code; need to determine whether we are doing power simulation or try to get exact p-value under the model.
-## for binary treatment, multiple(possibly not equal size of) strata
-## now for simple additive model with tau vector, 
-## and comparing difference in mean stat, wilcoxon stat and our new statistic
+# estimate quantile effect by quantreg package and treat this as a true effect
 
-power_sim_block = function(Z, Y,
-                           block,
-                           k, c,
-                           asym = TRUE, 
-                           tau = seq(0, 1, len = 3), # treatment effect for alternative distribution; will be a vector, corresponding to different additive cases
-                           methods.list.all,
-                           weight = NULL, 
-                           stat.null = NULL,
-                           Z.perm.all = NULL,
-                           null.max = 10^4, # length of the null distribution
-                           iter.sim = 100, # number for simulations
-                           alpha = 0.05){
+library(quantreg)
+
+Y.obs = pbs1$R01TMCRET
+Z.obs = pbs1$trt
+block.obs = factor(pbs1$STRA_BLOCK)
+
+N = length(Z.obs)
+B = length(levels(block.obs))
+block.levels = levels(block.obs)
+
+nb = rep(NA, B)
+mb = rep(NA, B)
+for(i in 1:B) {
+  nb[i] = sum(block.obs == block.levels[i])
+  mb[i] = sum( Z.obs[block.obs == block.levels[[i]]] )
+} 
+indx = c(0, cumsum(nb) )
+
+Z_block = as.factor( rep(1 : B, nb) )
+block.levels = levels(Z_block)
+
+Y0_tmp = rep(NA, N)
+Y1_tmp = rep(NA, N)
+trt_eff = rep(NA, N)
+
+for(u in 1 : B){
+  trt.b = Z.obs[Z_block == block.levels[u]]
+  Y.b = Y.obs[Z_block == block.levels[u]]
+  prob.seq.tmp = seq(0,1, length.out = length(Y.b))
+  tmp.data = data.frame(y = Y.b, trt = trt.b)
+ 
+  m.temp = rq(y ~ trt, tau = prob.seq.tmp, data = tmp.data)
+  trt_eff[ (indx[u] + 1) : indx[u+1] ] = m.temp$coef[2,]
+  Y0_tmp[ (indx[u] + 1) : indx[u+1] ] = Y.b - trt.b * m.temp$coef[2,]
+  Y1_tmp[ (indx[u] + 1) : indx[u+1] ] = Y.b + (1 - trt.b) * m.temp$coef[2,]
+} # note Y0 and Y1 is also arranged here by block orders
+
+any(is.na(Y0_tmp))
+any(is.na(Y1_tmp))
+min(trt_eff) > 0 # note there exists some negative treatment effect under 75%
+quantile(trt_eff)
+
+# function for power simulation, treating estimated quantile treatment effect as true
+
+power_sim_block_quant = function(Z, 
+                                 Y0, Y1, # potential outcome vectors; will estimate by ci.qte object
+                                 block,
+                                 k, 
+                                 c,
+                                 methods.list.all,
+                                 weight = NULL, 
+                                 iter.sim = 100, # number for simulations
+                                 alpha = 0.05
+){
   
   if(!is.factor(block)){
     block = as.factor(block)
   }  
-
-  ## STEP 1: generating null distribution, and record threshold ##
-  
-  thres = rep(NA, 2)
   
   N = length(Z)
   B = length(levels(block))
+  #  block.levels = levels(block)
   block.levels = levels(block)
   
   nb = rep(NA, B)
@@ -79,111 +121,90 @@ power_sim_block = function(Z, Y,
     mb[i] = sum( Z[block == block.levels[[i]]] )
   } 
   indx = c(0, cumsum(nb) )
+  Z_block = as.factor( rep(1 : B, nb) )
+  block.levels = levels(Z_block)
   
-  new_block = as.factor( rep(1 : B, nb) )
-  
-  if(asym == TRUE){  
-    fit_t = lm_robust(Y ~ Z, fixed_effects = ~block)
-    thres[1] = confint(fit_t, level = (1 - alpha * 2) )[2]  #asymptotic t-stat
-  } else {
-    
-    t.null.dist = rep(NA, null.max)
-    
-    ## generating potential outcomes; now, just for additive model; on the ** null distribution ** 
-    
-    Y0 = rep(NA, N)
-    for(i in 1:B){
-      trt.b = Z[block == block.levels[i]]
-      Y.temp = Y[block == block.levels[i]]
-      Y.temp[trt.b == 1] = Y.temp[trt.b == 1]       # null is fixed in this case(?)
-      Y0[ (indx[i] + 1) : indx[i+1] ] = Y.temp
-    }
-    Y1 = Y0
-    
-    ## generating assignment matrix  
-    
-    if(is.null(Z.perm.all)){
-      Z.perm = matrix(nrow = N, ncol = null.max)
-      for(iter in 1 : null.max){
-        for(i in 1:B){
-          Z.perm[(indx[i] + 1 ):indx[i+1],iter] = sample( c(rep(1, mb[i]), rep(0, nb[i] - mb[i])) )
-        }
-      }
-    }    else {       
-      Z.perm = Z.perm.all
-    }
-    
-    for(k in 1 : null.max){
-      Z.iter = Z.perm[, k]
-      Y.iter = Y0 * (1 - Z.iter) + Y1 * Z.iter
-      t.null.dist[k] = lm_robust(Y.iter ~ Z.iter, fixed_effects = ~new_block)$statistic
-    }
-    
-    thres[1] = sort(t.null.dist, decreasing = TRUE)[ floor(null.max * alpha) + 1]     
-  }
-  
-  ## for new statistic
-  
-  ms_list = mu_sigma_list(Z, block = block, methods.list.all = methods.list.all)
-  comb.null.dist = com_null_dist_block(Z, block = block, 
-                                       methods.list.all = methods.list.all, 
-                                       mu_sigma_list = ms_list,
-                                       null.max = null.max)
-  thres[2] = sort(comb.null.dist, decreasing = TRUE)[ floor(null.max * alpha) + 1]
-  
-  ## STEP 2: check the power ##
-  ### this will come from the true distribution; true treatment effect we assumed ###
+  result = rep(NA, 2)
   
   reject.all = matrix(NA, nrow = 2, ncol = iter.sim)
-  result = matrix(NA, nrow = length(tau), ncol = 2 )
   
-  for(o in 1 : length(tau)){ 
-    reject.all = matrix(NA, nrow = 2, ncol = iter.sim)
+  for(iter in 1 : iter.sim){
+    temp = rep(NA, 2)
     
-    for(iter in 1 : iter.sim){
-      
-      tau.sim = tau[o]
-      temp = rep(NA, 2)
-      
-      #generate assignment(Z) corresponding to block structure
-      
-      Z.sim = rep(NA, N)
-      for (i in 1 : length(nb) ){
-        Z.sim[ (indx[i] + 1) : indx[i+1] ] = sample(rep(c(0,1), times = c( nb[i] - mb[i] , 
-                                                                           mb[i]  ) ), 
-                                                    nb[i], replace = FALSE)
-      }
-      Z_block = as.factor( rep(1 : B, nb) )
-      block.levels.sim = levels(Z_block)
-      
-      # generate Y
-      
-      Y0.sim = rep(NA, N)
-      for(i in 1:B){
-        trt.b = Z.sim[Z_block == block.levels.sim[i]]
-        Y.temp = Y[Z_block == block.levels.sim[i]]
-        Y.temp[trt.b == 1] = Y.temp[trt.b == 1] - tau.sim   
-        Y0.sim[ (indx[i] + 1) : indx[i+1] ] = Y.temp
-      }
-      Y1.sim = Y0.sim + tau.sim
-      Y.sim = Z.sim * Y1.sim + (1 - Z.sim) * Y0.sim
-      
-      # calculate test statistics
-      
-      temp[1] = lm_robust(Y.sim ~ Z.sim, fixed_effects = ~Z_block)$statistic
-      temp[2] = pval_comb(Z = Z.sim, 
-                          Y = Y.sim,
-                          k = k, 
-                          c = c, 
-                          block = Z_block,
-                          methods.list.all = methods.list.all,
-                          null.max = 10^3,
-                          statistic = TRUE)[2]
-      
-      reject.all[,iter] = as.numeric( temp - thres > 0)
-      print(iter)
+    #generate assignment(Z) corresponding to block structure
+    
+    Z.sim = rep(NA, N)
+    for (i in 1 : length(nb) ){
+      Z.sim[ (indx[i] + 1) : indx[i+1] ] = sample(rep(c(0,1), times = c( nb[i] - mb[i] , 
+                                                                         mb[i]  ) ), 
+                                                  nb[i], replace = FALSE)
     }
-    result[o,] = rowMeans(reject.all)
-  }  
+    
+    Y.sim = Z.sim * Y1 + (1 - Z.sim) * Y0
+
+    # calculate test statistics and compare their p-values
+    
+    temp[1] = lm_robust(Y.sim ~ Z.sim, fixed_effects = ~Z_block)$p.value  
+    temp[2] = pval_comb(Z = Z.sim, 
+                        Y = Y.sim,
+                        k = k, 
+                        c = c, 
+                        block = Z_block,
+                        methods.list.all = methods.list.all,
+                        null.max = 10^3,
+                        statistic = FALSE)
+    
+    reject.all[,iter] = as.numeric( temp < alpha )
+    print(iter)
+  }
+  result[1,] = rowMeans(reject.all)
   print(result)
 }
+
+# test code
+
+power_sim_block_quant(Z = pbs1$trt, 
+                      Y0 = Y0_tmp, 
+                      Y1 = Y1_tmp,
+                      block = factor(pbs1$STRA_BLOCK), 
+                      k = length(pbs1$R01TMCRET),
+                      c = 0,
+                      methods.list.all = methods.list,
+                      iter.sim = 100,
+                      alpha = 0.05)
+
+# result: power for lm.robust: 0.98 ///// 
+# power for new rank sum statistic 
+# for the null such that maximum quantile effect is upper bounded by 0: 1
+# 75% quantile bounded by 0: 0
+# median quantile effect bounded by 0: 
+
+med = power_sim_block_quant(Z = pbs1$trt, 
+                      Y0 = Y0_tmp, 
+                      Y1 = Y1_tmp,
+                      block = factor(pbs1$STRA_BLOCK), 
+                      k = floor(1/2 * length(pbs1$R01TMCRET)),
+                      c = 0,
+                      methods.list.all = methods.list,
+                      iter.sim = 100,
+                      alpha = 0.05)
+
+threefour = power_sim_block_quant(Z = pbs1$trt, 
+                            Y0 = Y0_tmp, 
+                            Y1 = Y1_tmp,
+                            block = factor(pbs1$STRA_BLOCK), 
+                            k = floor(0.75 * length(pbs1$R01TMCRET)),
+                            c = 0,
+                            methods.list.all = methods.list,
+                            iter.sim = 100,
+                            alpha = 0.05)
+
+ninety = power_sim_block_quant(Z = pbs1$trt, 
+                                Y0 = Y0_tmp, 
+                                Y1 = Y1_tmp,
+                                block = factor(pbs1$STRA_BLOCK), 
+                                k = floor(0.9 * length(pbs1$R01TMCRET)),
+                                c = 0,
+                                methods.list.all = methods.list,
+                                iter.sim = 100,
+                                alpha = 0.05)
